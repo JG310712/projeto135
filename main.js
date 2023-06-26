e54c12aa1b9085c1badea37c4b4018f6f1e58542
joãoguilherme <112426149+JG310712@users.noreply.github.com>
2023-06-26 15:48:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+objects = [];
+status = "";
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
